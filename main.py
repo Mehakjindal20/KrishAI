32561d97e3ae25cc9afa9bfc7ffdb58c07d5a725
Mehak <mehak2045@gmail.com>
2026-05-16 16:15:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import pandas as pd
-from ydata_profiling import ProfileReport
 
-data=pd.read_csv("C:/Users/Taran/Desktop/KrishiAi/Raw Data Soil Fettility.csv")
-print(data.head(),"\n")
-print(data.info(),"\n")
+data = pd.read_csv("Raw Data Soil Fettility.csv")
 
-profile=ProfileReport(data)
-# profile.to_widgets()
-profile.to_file("Soil_Data.html")
-# we have no catagorical values
-print(data.describe(),"\n")
+print(data.head(), "\n")
+print(data.info(), "\n")
+
+# Removed ydata_profiling because it is not supported on Streamlit Cloud Python 3.14
+
+print(data.describe(), "\n")
 
 # There are no null values
 print(data.isna().sum())
+
 print(data.nunique())
+
 print(data['fertility'].value_counts())
-print(" 0- Describes low fertility\n 1- describes moderate fertility\n 2- describes good fertility")
+
+print("0 - Describes low fertility")
+print("1 - Describes moderate fertility")
+print("2 - Describes good fertility")
+# import pandas as pd
+# # from ydata_profiling import ProfileReport
+
+# data=pd.read_csv("C:/Users/Taran/Desktop/KrishiAi/Raw Data Soil Fettility.csv")
+# print(data.head(),"\n")
+# print(data.info(),"\n")
+
+# profile=ProfileReport(data)
+# # profile.to_widgets()
+# profile.to_file("Soil_Data.html")
+# # we have no catagorical values
+# print(data.describe(),"\n")
+
+# # There are no null values
+# print(data.isna().sum())
+# print(data.nunique())
+# print(data['fertility'].value_counts())
+# print(" 0- Describes low fertility\n 1- describes moderate fertility\n 2- describes good fertility")
 
